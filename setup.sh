fcff0ecd14a19a0ab4f29119d0cfbe357f483a09
rahilaode <laoderahilatansa@gmail.com>
2025-04-19 17:40:40 +0700

    add script to run services

--- setup.sh
+++ setup.sh
@@ -2,14 +2,6 @@
 docker compose -f ./setups/data_sources/flights_db/docker-compose.yml down -v
 docker compose -f ./setups/data_sources/flights_db/docker-compose.yml up --build --detach
 
-# Start data storage (minio)
-docker compose -f ./setups/minio/docker-compose.yml down -v
-docker compose -f ./setups/minio/docker-compose.yml up --build --detach
-
-# Start spark
-docker compose -f ./setups/spark/docker-compose.yml down -v
-docker compose -f ./setups/spark/docker-compose.yml up --build --detach
-
 # Start airflow
 docker compose -f ./setups/airflow/docker-compose.yml down -v
 docker compose -f ./setups/airflow/docker-compose.yml up --build --detach
